6c4be09ad1fbe1acf1d218297a09e2c10cd6486c
Julio Vinachi <julio899@gmail.com>
2025-02-21 20:23:33 -0400

    Add files via upload

---
+++ index.common.js
@@ -0,0 +1,6 @@
+/*!
+ * quasar-ui-investco v0.0.1
+ * (c) 2025 Julio Vinachi <julio899@gmail.com>
+ * Released under the MIT License.
+ */
+Object.defineProperty(exports,"__esModule",{value:!0});var vue=require("vue"),quasar=require("quasar"),Component={name:"InvestCo",setup:function(){return function(){return vue.h(quasar.QBadge,{class:"InvestCo",label:"InvestCo"})}}},version="0.0.1";function install(e){e.component(Component.name,Component)}exports.Component=Component,exports.install=install,exports.version=version;
