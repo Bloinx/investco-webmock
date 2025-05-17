de26c8403683212e89c5961cafc4242374cf9aa5
Julio Vinachi <julio899@gmail.com>
2025-05-17 16:23:01 -0400

    control

--- index.esm.js
+++
@@ -1,6 +0,0 @@
-/*!
- * quasar-ui-investco v0.0.1
- * (c) 2025 Julio Vinachi <julio899@gmail.com>
- * Released under the MIT License.
- */
-import{h}from"vue";import{QBadge}from"quasar";var Component={name:"InvestCo",setup:function(){return function(){return h(QBadge,{class:"InvestCo",label:"InvestCo"})}}},version="0.0.1";function install(n){n.component(Component.name,Component)}var VuePlugin=Object.freeze({__proto__:null,version:version,Component:Component,install:install});export{Component,VuePlugin as default,install,version};
